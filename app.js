80f1796c5708d5c5e8278a2b19f29ec2600709a7
GINN30 <adyagi14@gmail.com>
2023-05-05 17:24:01 +0700

    website informasi

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-$(function () {
+$(document).ready(function () {
+    // Add active class when off-canvas menu is shown
     $('.off-canvas').on('show.bs.off-canvas', function () {
-        $(this).addClass('active')
-        $(this).find('.off-canvas-content').addClass('active')
-    })
+        $(this).addClass('active');
+        $(this).find('.off-canvas-content').addClass('active');
+    });
 
+    // Remove active class when off-canvas menu is hidden
     $('.off-canvas').on('hide.bs.off-canvas', function () {
-        $(this).removeClass('active')
-        $(this).find('.off-canvas-content').removeClass('active')
-    })
+        $(this).removeClass('active');
+        $(this).find('.off-canvas-content').removeClass('active');
+    });
 
+    // Handle click events on off-canvas toggler
     $('.off-canvas-toggler').on('click', function (evt) {
-        evt.stopPropagation()
-        evt.preventDefault()
-
-        var elm = $(this).data('target')
-
-        if (!$(elm).hasClass('active')) {
-            $(elm).trigger('show.bs.off-canvas')
+        evt.stopPropagation();
+        evt.preventDefault();
+        var target = $(this).data('target');
+        if (!$(target).hasClass('active')) {
+            $(target).trigger('show.bs.off-canvas');
         } else {
-            $(elm).trigger('hide.bs.off-canvas')
+            $(target).trigger('hide.bs.off-canvas');
         }
-    })
+    });
 
+    // Handle click events on off-canvas content
     $('.off-canvas-content:not(.off-canvas-no-close)').on('click', function (evt) {
-        var elm = $(this).data('target')
-
-        if ($(elm).hasClass('active')) {
-            evt.stopPropagation()
-
-            $(elm).trigger('hide.bs.off-canvas')
+        var target = $(this).data('target');
+        if ($(target).hasClass('active')) {
+            evt.stopPropagation();
+            $(target).trigger('hide.bs.off-canvas');
         }
-    })
-})
+    });
+});
