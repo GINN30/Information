bea8f5c8b1b048c9ea1cf68f197ff328f8a6731a
GINN30 <adyagi14@gmail.com>
2023-05-05 15:18:08 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+$(function () {
+    $('.off-canvas').on('show.bs.off-canvas', function () {
+        $(this).addClass('active')
+        $(this).find('.off-canvas-content').addClass('active')
+    })
+
+    $('.off-canvas').on('hide.bs.off-canvas', function () {
+        $(this).removeClass('active')
+        $(this).find('.off-canvas-content').removeClass('active')
+    })
+
+    $('.off-canvas-toggler').on('click', function (evt) {
+        evt.stopPropagation()
+        evt.preventDefault()
+
+        var elm = $(this).data('target')
+
+        if (!$(elm).hasClass('active')) {
+            $(elm).trigger('show.bs.off-canvas')
+        } else {
+            $(elm).trigger('hide.bs.off-canvas')
+        }
+    })
+
+    $('.off-canvas-content:not(.off-canvas-no-close)').on('click', function (evt) {
+        var elm = $(this).data('target')
+
+        if ($(elm).hasClass('active')) {
+            evt.stopPropagation()
+
+            $(elm).trigger('hide.bs.off-canvas')
+        }
+    })
+})
