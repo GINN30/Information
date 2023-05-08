dcf6bc8a5602b776a8d7ef5fa4ba6b6f5dcfb5b6
GINN30 <adyagi14@gmail.com>
2023-05-08 22:48:40 +0700

    page dev

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
-$(document).ready(function () {
-    // Add active class when off-canvas menu is shown
-    $('.off-canvas').on('show.bs.off-canvas', function () {
-        $(this).addClass('active');
-        $(this).find('.off-canvas-content').addClass('active');
-    });
+var accordionButtons = document.querySelectorAll('.accordion-button');
 
-    // Remove active class when off-canvas menu is hidden
-    $('.off-canvas').on('hide.bs.off-canvas', function () {
-        $(this).removeClass('active');
-        $(this).find('.off-canvas-content').removeClass('active');
-    });
-
-    // Handle click events on off-canvas toggler
-    $('.off-canvas-toggler').on('click', function (evt) {
-        evt.stopPropagation();
-        evt.preventDefault();
-        var target = $(this).data('target');
-        if (!$(target).hasClass('active')) {
-            $(target).trigger('show.bs.off-canvas');
-        } else {
-            $(target).trigger('hide.bs.off-canvas');
-        }
-    });
-
-    // Handle click events on off-canvas content
-    $('.off-canvas-content:not(.off-canvas-no-close)').on('click', function (evt) {
-        var target = $(this).data('target');
-        if ($(target).hasClass('active')) {
-            evt.stopPropagation();
-            $(target).trigger('hide.bs.off-canvas');
-        }
+accordionButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        accordionButtons.forEach(function (otherButton) {
+            if (otherButton !== button) {
+                otherButton.classList.remove('active');
+            }
+        });
+        button.classList.toggle('active');
     });
 });
